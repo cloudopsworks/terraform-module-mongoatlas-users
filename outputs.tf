4b958dd9c9fbbe45326cf30f26269fda198ae2ad
Cristian Beraha <cristian@cloudops.works>
2024-12-05 13:34:14 -0300

    chore: fix outputs

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,13 @@
 output "users" {
   value = {
     for k, v in local.mongodb_credentials : k => {
-      username     = v.username
-      project_name = v.project_name
-      project_id   = v.project_id
-      engine       = v.engine
+      username            = v.username
+      project_name        = v.project_name
+      project_id          = v.project_id
+      engine              = v.engine
+      secrets_username    = aws_secretsmanager_secret.dbuser[k].name
+      secrets_password    = aws_secretsmanager_secret.randompass[k].name
+      secrets_credentials = aws_secretsmanager_secret.atlas_cred[k].name
     }
   }
 }
